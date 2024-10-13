5b655971b5312fd3cb5250bea785fedf16f5d554
liviabontempol <liviabontempolivia@gmail.com>
2024-10-13 20:37:45 -0300

    criando estrutura do codigo

---
+++ code/lds3/src/main/java/br/com/pucminas/lds3/Lds3Application.java
@@ -0,0 +1,13 @@
+package br.com.pucminas.lds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lds3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lds3Application.class, args);
+	}
+
+}
